6365a1b83fce0ef2b06da68ddb6c69095b3492e6
inhobaek-wj <inhobaek.wj@gmail.com>
2019-11-22 10:31:49 +0900

    initial commit

---
+++ src/test/java/com/jake/cattoystore/CatToyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.cattoystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatToyStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
