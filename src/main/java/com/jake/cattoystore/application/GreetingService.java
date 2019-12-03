a84480256ea63bb357af8adc5c5596c004dab9f2
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-03 11:10:32 +0900

    application layer uses domain layer

--- src/main/java/com/jake/cattoystore/application/GreetingService.java
+++ src/main/java/com/jake/cattoystore/application/GreetingService.java
@@ -1,14 +1,23 @@
 package com.jake.cattoystore.application;
 
+import com.jake.cattoystore.domain.Greeting;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
     public String getMessage(String name) {
-        if (name == null) {
-            return "Hello";
-        }
-        return "Hello, " + name;
+        // if (name == null) {
+        //     return "Hello";
+        // }
+        // return "Hello, " + name;
+
+        // application layer uses Greeting domain model,
+        // so when you need to change business logic, you can change domain model.
+        Greeting greeting = Greeting.builder()
+            .name(name)
+            .build();
+        return greeting.getMessage();
     }
 }
