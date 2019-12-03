b93911eedb80b008ab345984290d2c8af736ddc9
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-03 10:51:54 +0900

    change name of Greeting to GreetingDto, to use Greeting as Domain model

--- src/main/java/com/jake/cattoystore/controller/GreetingController.java
+++ src/main/java/com/jake/cattoystore/controller/GreetingController.java
@@ -1,7 +1,7 @@
 package com.jake.cattoystore.controller;
 
 import com.jake.cattoystore.application.GreetingService;
-import com.jake.cattoystore.dto.Greeting;
+import com.jake.cattoystore.dto.GreetingDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +15,12 @@ public class GreetingController {
     private GreetingService greetingService;
 
     @GetMapping("/hello")
-    public Greeting hello(
-                          @RequestParam(required = false) String name
-                          ) {
-        Greeting greeting = new Greeting();
-        greeting.setName("jake");
-        greeting.setMessage(greetingService.getMessage(name));
-        return greeting;
+    public GreetingDto hello(
+                             @RequestParam(required = false) String name
+                             ) {
+        GreetingDto greetingDto = new GreetingDto();
+        greetingDto.setName("jake");
+        greetingDto.setMessage(greetingService.getMessage(name));
+        return greetingDto;
     }
 }
---
+++ src/main/java/com/jake/cattoystore/dto/GreetingDto.java
@@ -0,0 +1,10 @@
+package com.jake.cattoystore.dto;
+
+import lombok.Data;
+
+@Data
+public class GreetingDto {
+
+    private String name;
+    private String message;
+}
