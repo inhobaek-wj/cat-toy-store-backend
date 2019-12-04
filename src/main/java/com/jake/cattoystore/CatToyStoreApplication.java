ecbf5122fcf85cecec8c56c7ccbcbc2ee1dbe1e7
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-04 13:40:18 +0900

    register JwtUtil as a bean & use @Value for system config variable

--- src/main/java/com/jake/cattoystore/CatToyStoreApplication.java
+++ src/main/java/com/jake/cattoystore/CatToyStoreApplication.java
@@ -2,7 +2,9 @@ package com.jake.cattoystore;
 
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
+import com.jake.cattoystore.util.JwtUtil;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,12 +12,21 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CatToyStoreApplication {
 
+    @Value("${jwt.secret}")
+    private String secret;
+
     public static void main(String[] args) {
         SpringApplication.run(CatToyStoreApplication.class, args);
     }
 
+
     @Bean
     public Mapper modelMapper() {
         return DozerBeanMapperBuilder.buildDefault();
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil(secret);
     }
 }
