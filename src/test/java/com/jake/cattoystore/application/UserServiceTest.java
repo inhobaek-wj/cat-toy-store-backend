523eb093ca95e3a9a180f1f0ca1620b17c7354d1
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-04 23:06:50 +0900

    throw exception in test using junit5, signin with not existed email

--- src/test/java/com/jake/cattoystore/application/UserServiceTest.java
+++ src/test/java/com/jake/cattoystore/application/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -15,6 +16,8 @@ import static org.mockito.Mockito.verify;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import com.jake.cattoystore.domain.User;
 import com.jake.cattoystore.domain.UserRepository;
@@ -67,6 +70,18 @@ public class UserServiceTest {
         User user = userService.authenticate("tester@example.com", "pass");
 
         assertThat(user).isNotNull();
+
+    }
+
+    // @Test(expected = EntityNotFoundException.class) <- junit 4.
+    @Test
+    public void authenticateWithNotExistedEmail() {
+        given(userRepository.findByEmail("x@example.com"))
+            .willThrow(new EntityNotFoundException());
+
+        // this is junit 5.
+        assertThrows(EntityNotFoundException.class,
+                     () -> userService.authenticate("x@example.com", "x"));
 
     }
 }
