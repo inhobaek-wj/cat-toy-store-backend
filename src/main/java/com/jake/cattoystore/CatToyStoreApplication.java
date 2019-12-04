785259a02f93bfe4529a9efb3438de6147e25726
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-04 21:55:34 +0900

    SecurityJavaConfig

--- src/main/java/com/jake/cattoystore/CatToyStoreApplication.java
+++ src/main/java/com/jake/cattoystore/CatToyStoreApplication.java
@@ -2,9 +2,7 @@ package com.jake.cattoystore;
 
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
-import com.jake.cattoystore.util.JwtUtil;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,21 +10,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CatToyStoreApplication {
 
-    @Value("${jwt.secret}")
-    private String secret;
 
     public static void main(String[] args) {
         SpringApplication.run(CatToyStoreApplication.class, args);
     }
 
-
     @Bean
     public Mapper modelMapper() {
         return DozerBeanMapperBuilder.buildDefault();
-    }
-
-    @Bean
-    public JwtUtil jwtUtil() {
-        return new JwtUtil(secret);
     }
 }
