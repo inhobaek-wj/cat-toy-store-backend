72bd74f26b042bf36213774f15504d036536edad
inhobaek-wj <inhobaek.wj@gmail.com>
2019-12-04 17:40:41 +0900

    POST /users, signup()

---
+++ src/main/java/com/jake/cattoystore/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.jake.cattoystore.controller;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import com.github.dozermapper.core.Mapper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping("/users")
+    public ResponseEntity<?> signup() throws URISyntaxException {
+        URI location = new URI("/");
+        return ResponseEntity.created(location).build();
+    }
+
+}
---
+++ src/test/java/com/jake/cattoystore/controller/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.jake.cattoystore.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.refEq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+// it takes a little bit long to test controller.
+
+@ExtendWith(SpringExtension.class) // junit5 <- @RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+@ActiveProfiles("myTest")
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void singup() throws Exception {
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"tester\","
+                                 + "\"email\":\"tester@example.com\","
+                                 + " \"password\":\"pass\"}")
+                        )
+            .andExpect(status().isCreated());
+    }
+
+}
